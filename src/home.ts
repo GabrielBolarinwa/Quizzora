251dbcb987d7bd32f21ffc81e7a00d7faad55bed
Gabriel <gabibola955@gmail.com>
2026-05-08 22:45:40 +0100

    Improved Homepage Last Result proper functionality and accessibility features Image optimizations for home page Performance optimizations for LCP and FCP Improved colour contrasts on light and dark modes

--- src/home.ts
+++ src/home.ts
@@ -1,112 +1,119 @@
-import { CircleX, createIcons, SunMoon } from "lucide";
-import type { Result, Theme } from "./types";
+import {CircleX, createIcons, SunMoon} from "lucide";
+import type {Result, Theme} from "./types";
 
 createIcons({
-  icons: {
-    SunMoon,
-    CircleX
-  }
+    icons: {
+        SunMoon,
+        CircleX
+    }
 });
 
 document.getElementById("themeButton")?.addEventListener("click", () => {
-  if (document.body.classList.contains("light")) setTheme("Dark");
-  else if (document.body.classList.contains("dark")) setTheme("Auto");
-  else setTheme("Light");
-}, { once: true });
+    if (document.body.classList.contains("light")) setTheme("Dark");
+    else if (document.body.classList.contains("dark")) setTheme("Auto");
+    else setTheme("Light");
+});
 
 let themeSetting = localStorage.getItem("themeSetting") as Theme;
 
 function setTheme(theme: Theme) {
-  themeSetting = theme;
-  if (themeSetting === "Dark") {
-    if (document.body.classList.contains("light")) {
-      document.body.classList.replace("light", "dark");
-    } else {
-      document.body.classList.add("dark");
+    themeSetting = theme;
+    if (themeSetting === "Dark") {
+        if (document.body.classList.contains("light")) {
+            document.body.classList.replace("light", "dark");
+        } else {
+            document.body.classList.add("dark");
+        }
+    } else if (themeSetting == "Light") {
+        if (document.body.classList.contains("dark")) {
+            document.body.classList.replace("dark", "light");
+        } else {
+            document.body.classList.add("light");
+        }
+    } else if (themeSetting == "Auto") {
+        document.body.classList.remove("light", "dark");
     }
-  } else if (themeSetting == "Light") {
-    if (document.body.classList.contains("dark")) {
-      document.body.classList.replace("dark", "light");
-    } else {
-      document.body.classList.add("light");
-    }
-  } else if (themeSetting == "Auto") {
-    document.body.classList.remove("light", "dark");
-  }
-  
-  localStorage.setItem("themeSetting", themeSetting);
+    
+    localStorage.setItem("themeSetting", themeSetting);
 }
 
 setTheme(themeSetting);
 
 const sectionTitle = document.querySelector(
-  ".section_header"
+    ".section_header"
 ) as HTMLDivElement;
 const featureCards = document.querySelectorAll(
-  ".feature-card"
+    ".feature_card"
 ) as NodeListOf<HTMLDivElement>;
 
 function addAnimationClass(element: HTMLElement) {
-  element.classList.add("opacity-0");
-  const animationObserver = new IntersectionObserver((entries) => {
-    entries.forEach((e) => {
-      let entry = e.target as HTMLElement;
-      if (e.isIntersecting) {
-        let animationClass = entry.getAttribute("data-animation") as string;
-        entry.classList.replace("opacity-0", animationClass);
-      }
-    });
-  });
-  animationObserver.observe(element);
+    element.classList.add("opacity-0", "invisible");
+    const animationObserver = new IntersectionObserver((entries) => {
+        
+        entries.forEach((e) => {
+            let entry = e.target as HTMLElement;
+            if (e.isIntersecting) {
+                let animationClass = entry.getAttribute("data-animation") as string;
+                entry.classList.remove("opacity-0", "invisible");
+                entry.classList.add(animationClass)
+            }
+        });
+    }, {threshold: 0.6});
+    animationObserver.observe(element);
 }
 
 window.addEventListener("load", () => {
-  featureCards.forEach((card) => {
-    addAnimationClass(card);
-  });
-  addAnimationClass(sectionTitle);
+    featureCards.forEach((card) => {
+        addAnimationClass(card);
+    });
+    addAnimationClass(sectionTitle);
 });
 
 const lastQuiz = localStorage.getItem("lastQuiz");
 if (lastQuiz) {
-  let lastQuizData = JSON.parse(lastQuiz) as Result;
-  let fullLastResult = document.createElement("div");
-  fullLastResult.classList.add("full_last_result");
-  fullLastResult.innerHTML = `
+    let lastQuizData = JSON.parse(lastQuiz) as Result;
+    let fullLastResult = document.createElement("div");
+    fullLastResult.classList.add("full_last_result", "animate-zoom-in");
+    fullLastResult.innerHTML = `
+<button class="absolute top-3 right-3" aria-label="Close Last Result" onclick="this.parentElement.style.display = 'none'"><span data-lucide="circle-x"></span></button>
   <div class="result_meta">
-<p>${lastQuizData.category} Quiz</p>
+<h2>${lastQuizData.categoryText} Quiz</h2>
 <p>Difficulty: ${lastQuizData.difficulty}</p>
 <p>You scored ${lastQuizData.score} out of ${lastQuizData.total}</p>
 </div>
-<div class="result_breakdown">
-<h4>Result Breakdown</h4>
-<ul>
+<details class="result_breakdown" open>
+<summary>Result Breakdown</summary>
+<ul class="max-h-[50vh] overflow-y-auto">
 </ul>
-</div>
+</details>
   `;
-  const resultBreakdownList = fullLastResult.querySelector(".result_breakdown ul");
-  lastQuizData.breakdown.forEach(breakdown => {
-    let resultBreakdownListItem = document.createElement("li");
-    resultBreakdownListItem.textContent = `Question ${breakdown.question}: ${breakdown.isCorrect ? `Correct ✅` : `Incorrect ❌ (Correct Answer: ${breakdown.correctAnswer})`}`;
-    breakdown.isCorrect ? resultBreakdownListItem.className = "correct" : resultBreakdownListItem.className = "incorrect";
-    resultBreakdownList?.appendChild(resultBreakdownListItem);
-  });
-  let resultWidget = document.createElement("div");
-  resultWidget.classList.add("last_result_widget");
-  resultWidget.innerHTML = `
-    Last Result: ${lastQuizData.score} / ${lastQuizData.total} out of ${lastQuizData.total} <button onclick="this.parentElement.style.display='none'"><span data-lucide="circle-x"></span></button>
+    const resultBreakdownList = fullLastResult.querySelector(".result_breakdown ul");
+    lastQuizData.breakdown.forEach((breakdown, index) => {
+        let resultBreakdownListItem = document.createElement("li");
+        resultBreakdownListItem.textContent = `Question ${index + 1}: ${breakdown.isCorrect ? `Correct ✅` : `Incorrect ❌ (Correct Answer: ${breakdown.correct})`}`;
+        breakdown.isCorrect ? resultBreakdownListItem.className = "correct" : resultBreakdownListItem.className = "incorrect";
+        resultBreakdownList?.appendChild(resultBreakdownListItem);
+    });
+    let resultWidget = document.createElement("div");
+    resultWidget.classList.add("last_result_widget");
+    resultWidget.innerHTML = `
+    <span id="score">Last Result: ${lastQuizData.score} out of ${lastQuizData.total}</span> <button onclick="localStorage.removeItem('lastQuiz');this.parentElement.style.display='none'" aria-label="Close Last Result"><span data-lucide="circle-x"></span></button>
   `;
-  resultWidget.onclick = () => {
-    displayFullResult();
-  };
-  document.body.append(fullLastResult, resultWidget);
+    (resultWidget.querySelector("#score") as HTMLSpanElement).onclick = () => {
+        displayFullResult();
+    };
+    document.body.append(fullLastResult, resultWidget);
+    createIcons({
+        icons: {CircleX}
+    });
 }
 
 function displayFullResult() {
-  const fullResult = document.querySelector(".full_last_result") as HTMLDivElement;
-  if (fullResult) {
-    fullResult.style.display = "block";
-  }
+    const fullResult = document.querySelector(".full_last_result") as HTMLDivElement;
+    if (fullResult) {
+        fullResult.style.display = "block";
+    }
 }
 
 (document.getElementById("yearText") as HTMLSpanElement).innerText = new Date().getFullYear().toString();
+
