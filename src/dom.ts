914049b0ca4f3d96ce19487123243e8aac7b48bf
Gabriel <gabibola955@gmail.com>
2026-08-19 23:56:25 +0100

    Split dom reference file

---
+++ src/dom.ts
@@ -0,0 +1,45 @@
+export const categoriesDropdown = document.getElementById(
+    "questionCategories",
+) as HTMLSelectElement;
+export const questionNumber = document.getElementById(
+    "questionNumber",
+) as HTMLInputElement;
+export const quizForm = document.getElementById("quizForm") as HTMLFormElement;
+export const startQuizButton = document.getElementById(
+    "startQuiz",
+) as HTMLButtonElement;
+export const exitQuizButton = document.getElementById(
+    "exitQuiz",
+) as HTMLButtonElement;
+export const nextButton = document.getElementById(
+    "nextQuestion",
+) as HTMLButtonElement;
+export const previousButton = document.getElementById(
+    "previousQuestion",
+) as HTMLButtonElement;
+export const optionsList = document.getElementById(
+    "optionList",
+) as HTMLDivElement;
+export const quitQuizButton = document.getElementById(
+    "quitQuizButton",
+) as HTMLButtonElement;
+export const replayQuizButton = document.getElementById(
+    "replayQuizButton",
+) as HTMLButtonElement;
+export const requestSubmitButton = document.getElementById(
+    "requestSubmit",
+) as HTMLButtonElement;
+export const timerEl = document.getElementById("timer") as HTMLParagraphElement;
+export const questionTextEl = document.getElementById(
+    "questionText",
+) as HTMLHeadingElement;
+export const questionCounterEl = document.getElementById(
+    "currentQuestion",
+) as HTMLSpanElement;
+export const totalQuestionEl = document.getElementById(
+    "totalQuestions",
+) as HTMLSpanElement;
+export const hardDifficulty = document.getElementById("hard") as HTMLInputElement;
+export const easyDifficulty = document.getElementById("easy") as HTMLInputElement;
+export const mediumDifficulty = document.getElementById("medium") as HTMLInputElement;
+export const quizFetchError = document.getElementById("quizFetchError") as HTMLParagraphElement
